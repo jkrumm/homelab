1c7aab2a507fc0ef554fc375ef9dc01d5c5231fb
jkrumm <jkrumm@pm.me>
2026-02-28 22:54:36 +0100

    cleanup: remove debugging test route

--- homelab-api/src/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from 'elysia'
-
-export const testRoute = new Elysia().get(
-  '/testroutetest',
-  () => ({ test: 'ok' }),
-  {
-    detail: { tags: ['Test'], summary: 'Test route' },
-  },
-)
